d6919444f933c344b25975a7ed5ad4b89d22c038
Graham Lea <graham.lea@belmonttechnology.com.au>
2018-04-26 21:15:03 +1000

    Added ImmutableList<E>.plus(Collection<E>) and improved the variance of collections

--- src/main/kotlin/com/grahamlea/forbiddenisland/Collections.kt
+++ src/main/kotlin/com/grahamlea/forbiddenisland/Collections.kt
@@ -2,18 +2,16 @@ package com.grahamlea.forbiddenisland
 
 import java.util.*
 
-data class ImmutableList<E>(private val inner:List<E>) : List<E> by inner
-data class ImmutableMap<K, V>(private val inner: Map<K, V>) : Map<K, V> by inner
-
-fun <E> List<E>.immutable() = this as? ImmutableList<E> ?: ImmutableList(this)
-fun <K, V> Map<K, V>.immutable() = this as? ImmutableMap<K, V> ?: ImmutableMap(this)
-
-infix operator fun <E> ImmutableList<E>.plus(e: E) = ImmutableList(this as List<E> + e)
-infix operator fun <K, V> ImmutableMap<K, V>.plus(p: Pair<K, V>) = ImmutableMap(this as Map<K, V> + p)
-
+data class ImmutableList<out E>(private val inner:List<E>) : List<E> by inner
 fun <T> immutableListOf(element: T): ImmutableList<T> = java.util.Collections.singletonList(element).immutable()
 fun <T> immutableListOf(vararg elements: T): ImmutableList<T> = listOf(*elements).immutable()
+infix operator fun <E> ImmutableList<E>.plus(e: E): ImmutableList<E> = ImmutableList(this as List<E> + e)
+infix operator fun <E> ImmutableList<E>.plus(es: Collection<E>): ImmutableList<E> = ImmutableList(this as List<E> + es)
+fun <E> List<E>.immutable() = this as? ImmutableList<E> ?: ImmutableList(this)
+
+data class ImmutableMap<K, out V>(private val inner: Map<K, V>) : Map<K, V> by inner
+fun <K, V> Map<K, V>.immutable() = this as? ImmutableMap<K, V> ?: ImmutableMap(this)
+infix operator fun <K, V> ImmutableMap<K, V>.plus(p: Pair<K, V>): ImmutableMap<K, V> = ImmutableMap(this as Map<K, V> + p)
 
 inline fun <reified T : Enum<T>> shuffled(random: Random = Random(), count: Int = enumValues<T>().size): ImmutableList<T>
         = enumValues<T>().toList().shuffled(random).take(count).immutable()
-
