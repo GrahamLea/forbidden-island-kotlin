17c4330671cece7bd38397e47d1fe7d243ba17ac
Graham Lea <graham.lea@belmonttechnology.com.au>
2018-05-28 22:05:33 +1000

    Replaced repetition in TreasureDeckTest cases with @RepeatedTest

--- src/test/kotlin/com/grahamlea/forbiddenisland/TreasureDeckTest.kt
+++ src/test/kotlin/com/grahamlea/forbiddenisland/TreasureDeckTest.kt
@@ -3,29 +3,26 @@ package com.grahamlea.forbiddenisland
 import com.grahamlea.forbiddenisland.Treasure.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.RepeatedTest
 
 @DisplayName("treasure deck")
 class TreasureDeckTest {
-    @Test
+    @RepeatedTest(10)
     fun `has 3 helicopter lift cards`() {
         assertThat(TreasureDeck.newShuffledDeck().filter { it is HelicopterLiftCard }).hasSize(3)
-        assertThat(TreasureDeck.newShuffledDeck().filter { it is HelicopterLiftCard }).hasSize(3)
     }
 
-    @Test
+    @RepeatedTest(10)
     fun `has 2 sandbags cards`() {
         assertThat(TreasureDeck.newShuffledDeck().filter { it is SandbagsCard }).hasSize(2)
-        assertThat(TreasureDeck.newShuffledDeck().filter { it is SandbagsCard }).hasSize(2)
     }
 
-    @Test
+    @RepeatedTest(10)
     fun `has 3 Waters Rise! cards`() {
         assertThat(TreasureDeck.newShuffledDeck().filter { it is WatersRiseCard }).hasSize(3)
-        assertThat(TreasureDeck.newShuffledDeck().filter { it is WatersRiseCard }).hasSize(3)
     }
 
-    @Test
+    @RepeatedTest(10)
     fun `has 5 of each treasure card`() {
         val treasureCardCounts = TreasureDeck.newShuffledDeck().filter { it is TreasureCard }.groupingBy { (it as TreasureCard).treasure }.eachCount()
         assertThat(treasureCardCounts).isEqualTo(mapOf(CrystalOfFire to 5, EarthStone to 5, OceansChalice to 5, StatueOfTheWind to 5))
