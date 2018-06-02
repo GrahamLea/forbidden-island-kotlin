3af3b7b74c7290e646e87c7b9077026e425ff70d
Graham Lea <graham.lea@belmonttechnology.com.au>
2018-06-02 23:04:33 +1000

    Added a test for Position.allPositions

--- src/test/kotlin/com/grahamlea/forbiddenisland/GameMapTest.kt
+++ src/test/kotlin/com/grahamlea/forbiddenisland/GameMapTest.kt
@@ -30,6 +30,18 @@ class GameMapTest {
     }
 
     @Test
+    fun `allPositions contains them all (and no more)`() {
+        assertThat(Position.allPositions).containsExactlyInAnyOrder(
+            Position(3, 1), Position(4, 1),
+            Position(2, 2), Position(3, 2), Position(4, 2), Position(5, 2),
+            Position(1, 3), Position(2, 3), Position(3, 3), Position(4, 3), Position(5, 3), Position(6, 3),
+            Position(1, 4), Position(2, 4), Position(3, 4), Position(4, 4), Position(5, 4), Position(6, 4),
+            Position(2, 5), Position(3, 5), Position(4, 5), Position(5, 5),
+            Position(3, 6), Position(4, 6)
+        )
+    }
+
+    @Test
     fun `new random map should have 24 distinct locations`() {
         assertThat(GameMap.newRandomMap().mapSites.map { it.location }.distinct()).hasSize(Location.values().size)
     }
