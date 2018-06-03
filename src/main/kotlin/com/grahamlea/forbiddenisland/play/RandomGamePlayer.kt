5db863467a555b96563916626b416e5b85fae7ca
Graham Lea <graham.lea@belmonttechnology.com.au>
2018-06-03 21:35:07 +1000

    Converted RandomGamePlayer's Context to an inner class to give a better example of how to lay out a GamePlayer implementation

--- src/main/kotlin/com/grahamlea/forbiddenisland/play/RandomGamePlayer.kt
+++ src/main/kotlin/com/grahamlea/forbiddenisland/play/RandomGamePlayer.kt
@@ -6,13 +6,13 @@ import java.util.*
 
 class RandomGamePlayer(private val random: Random = Random(27697235L)): GamePlayer {
 
-    override fun newContext(game: Game): GamePlayer.GamePlayContext {
-        return object: GamePlayer.GamePlayContext {
-            override fun selectNextAction(): GameAction =
-                game.gameState.availableActions.let { availableActions ->
-                    availableActions[random.nextInt(availableActions.size)]
-                }
-        }
+    override fun newContext(game: Game): GamePlayer.GamePlayContext = Context(game)
+
+    inner class Context(private val game: Game) : GamePlayer.GamePlayContext {
+        override fun selectNextAction(): GameAction =
+            game.gameState.availableActions.let { availableActions ->
+                availableActions[random.nextInt(availableActions.size)]
+            }
     }
 
     companion object {
