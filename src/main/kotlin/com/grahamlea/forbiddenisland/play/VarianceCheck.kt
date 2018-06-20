9dfe251ab962bee22f2cd47fc5dcabf0f5c436e4
Graham Lea <graham.lea@belmonttechnology.com.au>
2018-06-20 21:19:28 +1000

    Make the Variance Check multi-threaded

--- src/main/kotlin/com/grahamlea/forbiddenisland/play/VarianceCheck.kt
+++ src/main/kotlin/com/grahamlea/forbiddenisland/play/VarianceCheck.kt
@@ -3,6 +3,9 @@ package com.grahamlea.forbiddenisland.play
 import com.grahamlea.forbiddenisland.AdventurersWon
 import com.grahamlea.forbiddenisland.StartingFloodLevel
 import java.util.*
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import kotlin.math.max
 
 object VarianceCheck {
 
@@ -11,18 +14,31 @@ object VarianceCheck {
     @JvmStatic
     fun main(args: Array<String>) {
         val batchNumbers = 1..5
+        val numberOfThreads = max(1, Runtime.getRuntime().availableProcessors() - 1)
+        val fixedThreadPool = Executors.newFixedThreadPool(numberOfThreads)
+        val results = fixedThreadPool.invokeAll(
+            generateSequence(1) { it * 2 }.takeWhile { it < 40_000 }.toList().reversed().map { limit ->
+                Callable {
+                    println("Testing batches of $limit")
+                    val random = Random(88)
+                    limit to batchNumbers.map { getAverageActionsAndWinRate(limit, gamePlayer, random) }
+                }
+            }
+        )
+        fixedThreadPool.shutdown()
         println("Games per batch,${batchNumbers.map { "Batch $it avg. actions" }.joinToString(",")},${batchNumbers.map { "Batch $it win rate" }.joinToString(",")}")
-        generateSequence(1) { it * 2 }.takeWhile { it < 10_000 }.forEach { limit ->
-            val random = Random(88)
-            val results = batchNumbers.map { getAverageActionsAndWinRate(limit, gamePlayer, random) }
-            println("${"%4d".format(limit)},${results.map { it.first }.joinToString(",")},${results.map { "%4.1f%%".format(it.second) }.joinToString(",")}")
+        for (result in results.reversed()) {
+            val (limit, actionCountsAndWinRatios) = result.get()
+            val actionCountsList = actionCountsAndWinRatios.map { it.first }.joinToString(",")
+            val winRatiosList = actionCountsAndWinRatios.map { "%4.1f%%".format(it.second) }.joinToString(",")
+            println("${"%4d".format(limit)},$actionCountsList,$winRatiosList")
         }
     }
 
     private fun getAverageActionsAndWinRate(limit: Int, gamePlayer: GamePlayer, random: Random): Pair<Int, Float> {
-        var runs = 0
         var totalActions = 0
         var totalWins = 0
+        var runs = 0
         while (runs < limit) {
             runs++
             val gameState = playGame(gamePlayer, 4, StartingFloodLevel.Novice, random).gameState
